fd1b660638763d5633aa6049f2fd96920484ca99
Daniel224455 <danalexgro@gmail.com>
2025-12-14 15:04:27 +0100

    resolve build

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ set -e
 . build_common.sh
 # not actually GCC5; it's GCC7 on Ubuntu 18.04.
 ./build_bootshim.sh
-rm workspace/UEFI.bin
+#rm workspace/UEFI.bin
 GCC5_AARCH64_PREFIX=aarch64-linux-gnu- build -j$(nproc) -s -n 0 -a AARCH64 -t GCC5 -p T8010Pkg/Devices/iPhone7.dsc||exit 1
 cat BootShim/BootShim.bin workspace/Build/T8010Pkg/DEBUG_GCC5/FV/T8010PKG_UEFI.fd > workspace/UEFI.bin||exit 1
